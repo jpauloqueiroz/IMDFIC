e8498ceacc4532dbee4222b3da021541bd983d35
João Paulo Queiroz <jpauloqueiroz@users.noreply.github.com>
2020-06-16 17:53:46 -0300

    poo aula 7

---
+++ src/Prog_Orientada_a_Objetos/aula07/Automovel.java
@@ -0,0 +1,7 @@
+package Prog_Orientada_a_Objetos.aula07;
+
+public class Automovel extends Terrestre {
+    private String cor;
+    private int numPortas;
+    private String placa;
+}
---
+++ src/Prog_Orientada_a_Objetos/aula07/Transporte.java
@@ -0,0 +1,5 @@
+package Prog_Orientada_a_Objetos.aula07;
+
+public class Transporte {
+    private int capacidade;
+}
